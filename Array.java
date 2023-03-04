c4591cfd8c55f6ea1b3e21a6c4e3f973bca9c16d
Uri_Klevcov <a-08-137.mail.ru>
2023-03-04 17:41:06 +0600

    corrected

--- Array.java
+++ Array.java
@@ -4,9 +4,9 @@ import java.util.HashSet;
 public class Array {
     public static void main(String[] args) {
         
-        int[] nums1 = new int[] { 8, 8, 0, 4, 5, 8, 7, 7 };
+        int[] nums1 = new int[] { 9, 8, 8, 0, 5, 8, 7, 7 };
         //                        0  1  2  3  4  5  6  7
-        int[] nums2 = new int[] { 8, 8, 0, 8, 5, 8, 7, 7 };
+        int[] nums2 = new int[] { 0, 8, 8, 0, 0, 0, 0 };
 
         int maxSize = FindMaxSize(nums1, nums2);     
         System.out.println(maxSize);
@@ -21,17 +21,18 @@ public class Array {
             for (int j = 0; j < nums2.length; j++) {
                 if (nums1[i] == nums2[j]) {
                     item = true;
-                    if (i > 0 && nums1[i - 1] == nums1[i]) break;
+                    ArrayList<Integer> array = new ArrayList<>();
+                    array.add(j);
+                    dic.add(array);
                     for (ArrayList<Integer> col : dic) {
-                        if (col.contains(j - 1)) {
+                        if (j > 0 && nums2[j - 1] == nums2[j] && i > 0 && nums1[i - 1] != nums1[i]);
+                        else if (col.contains(j - 1)) {
                             col.add(j);
                             maxSize = col.size() > maxSize ? col.size() : maxSize;
                         }
                     }
-                    ArrayList<Integer> array = new ArrayList<>();
-                    array.add(j);
-                    dic.add(array);
                 }
+                maxSize = item && maxSize < 1 ? 1 : maxSize;
             }
         }
         return maxSize;
